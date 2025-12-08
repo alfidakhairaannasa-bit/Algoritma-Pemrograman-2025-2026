b7a13c6ebdbe0a05bfc8c57e1f860918b0ea0610
alfidakhairaannasa-bit <alfidakhairaannasa@gmail.com>
2025-12-08 10:50:28 +0700

    Menambahkan NIM Array

---
+++ c atau c++/array.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string nim;
+    cout << "Masukkan NIM: ";
+    cin >> nim;
+
+    vector<int> digit;
+
+    for (char c : nim) {
+        digit.push_back(c - '0');
+    }
+
+    int total = 0;
+    for (int d : digit) {
+        total += d;
+    }
+
+    int maks = *max_element(digit.begin(), digit.end());
+    int minm = *min_element(digit.begin(), digit.end());
+    double rata = (double) total / digit.size();
+
+    reverse(digit.begin(), digit.end());
+
+    cout << "\nArray digit: ";
+    for (int d : digit) {
+        cout << d << " ";
+    }
+
+    cout << "\nTotal: " << total;
+    cout << "\nMaksimum: " << maks;
+    cout << "\nMinimum: " << minm;
+    cout << "\nRata-rata: " << rata;
+
+    return 0;
+}
---
+++ php/array.php
@@ -0,0 +1,28 @@
+<?php
+
+$nim = readline("Masukkan NIM: ");
+
+$digit = [];
+
+for ($i = 0; $i < strlen($nim); $i++) {
+    $digit[] = (int)$nim[$i];
+}
+
+$total = array_sum($digit);
+$max = max($digit);
+$min = min($digit);
+$rata = $total / count($digit);
+
+$reverse = array_reverse($digit);
+
+echo "\nArray Reverse: ";
+foreach ($reverse as $d) {
+    echo $d . " ";
+}
+
+echo "\nTotal: $total";
+echo "\nMaksimum: $max";
+echo "\nMinimum: $min";
+echo "\nRata-rata: $rata";
+
+?>
